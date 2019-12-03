dadd5172f228afe6bc143dc6b38ecfa779120ddf
Oskari Ahonen <ahonenoskari@gmail.com>
2019-12-03 18:56:14 +0200

    tehtävä 11

--- src/tehtava11/BinarySearch.java
+++ src/tehtava11/BinarySearch.java
@@ -2,45 +2,82 @@ package tehtava11;
 
 public class BinarySearch {
 	
-	private int[] arvot = {1, 2, 3, 4, 5, 6, 7};
+	private int[] arvot;
 	private int määrä;
 	
 	public BinarySearch() {
-	//	arvot = new int[7];
+		arvot = new int[10];
 		
-		määrä = 7;
+		määrä = 0;
 	}
 	
-	public boolean insert(int arvo) {
-		
-		
-		
-		return false;
+	public int binaryInsert(int arvo) {
+		if (määrä == 0) {
+		      return 0;
+		    }
+
+		    int min = 0;
+		    int max = määrä - 1;
+
+		    while (true) {
+		      int index = (max + min) / 2;
+		      if (min == index) {
+		        if (arvot[index] > arvo) {
+		          return index;
+		        }
+		      }
+		      if (arvot[index] < arvo) {
+		        min = index + 1;
+		        if (min > max) {
+		          return index += 1;
+		        }
+		      } else if (min > max) {
+		        return index;
+		      } else {
+		        max = index - 1;
+		      }
+		    }
 	}
 	
-	public int find(int arvo) {
+	public void insert(int arvo) {
 		
-		if (määrä!=0) {
-			
-		int max = määrä-1;
-		int min = 0;
-		
-		while (max-min>1) {
-			int mid = 0;
-		System.out.println(mid);
-		System.out.println(min);
-		System.out.println(max);
-		if (arvo <= arvot[mid]) {
-			max = mid;
-			mid = arvot[max/2];
+		if (määrä == 10) {
+			System.out.println("taulu täynnä.");
 		} else {
-			min = mid;
-			mid = arvot[mid + (max-min)];
+			int in = binaryInsert(arvo);
+			for (int i = määrä; i > in; i--) {
+				arvot[i] = arvot[i-1];
+			}
+			arvot[in] = arvo;
+			määrä++;
 		}
+	}
+	
+	public int find(int arvo, int min, int max) {
+		
+		if (max >= min) {
+			int mid = min + (max-min)/2;
+			
+			if (arvot[mid]==arvo) {
+				return mid;
+			}
+			
+			if (arvot[mid]>arvo) {
+				return find(arvo, min, mid-1);
+			}
+			
+			return find(arvo, mid+1, max);
 		
 		}
-		}
-		return 0;
+		return -1;
+	}
+	
+	public int getMaara() {
+		return määrä-1;
+	}
+	
+	public int[] getArvot() {
+		return arvot;
 	}
 
 }
--- src/tehtava11/Main.java
+++ src/tehtava11/Main.java
@@ -1,13 +1,46 @@
 package tehtava11;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		BinarySearch s = new BinarySearch();
 		
-		System.out.println(s.find(6));
+		boolean running = true;
 		
+		while (running) {
+			System.out.println("1. Lisää numero.");
+			System.out.println("2. Etsi numeron indexi.");
+			System.out.println("0. Lopeta.");
+			Scanner in = new Scanner(System.in);
+			int input = in.nextInt();
+			in.nextLine();
+			
+			switch(input) {
+			case 1:
+				System.out.println("Anna luku.");
+				input = in.nextInt();
+				in.nextLine();
+				s.insert(input);
+				for (int i : s.getArvot()) {
+					if (i!=0) {
+					System.out.println(i);
+					}
+				}
+				break;
+			case 2:
+				System.out.println("Anna etsittävä luku.");
+				input = in.nextInt();
+				in.nextLine();
+				System.out.println("Luvun indexi: "+s.find(input, 0, s.getMaara()));
+				break;
+			case 0:
+				running=false;
+				break;
+			}
+		}
 	}
 
 }
